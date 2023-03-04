edb5472c9118cdb34ea7b45e0572478625966e03
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-03-04 13:33:55 +0530

    Add files via upload

--- expenseTracker.js
+++ expenseTracker.js
@@ -7,24 +7,94 @@ form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 itemList.addEventListener('click', editItem);
 var editingId = null;
+
 function addItem(e) {
     e.preventDefault();
     console.log(1);
-    var a = [];
     const user = {
         amn: e.target.amt.value,
         dec: e.target.dsc.value,
         crt: e.target.ctg.value,
        
     }
-    a.push(user);
 
-    var newItem = document.getElementById('amount').value;
-    var newDes = document.getElementById('des').value;
-    var newCat = document.getElementById('cat').value;
+    //showData(user)
+   // document.form.reset();
+    //if (editingId === null) {
+      //  localStorage.setItem(user.ides, JSON.stringify(user));
+        //editingId=user.ides;
+    //} else {
+      //  localStorage.setItem(editingId, JSON.stringify(user));
+        //editingId = null;
+    //}
+
+    axios
+    .post("https://crudcrud.com/api/8b1d5eae718848d2985828db9e6952c0/expenseData",user)
+   .then(res =>showData(res.data))
+   .catch(err=>console.log(err));
+    console.log(user);
+
+form.reset();
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You sure?')) {
+            var li = e.target.parentElement;
+            
+            console.log(li)
+            axios
+    .delete(`https://crudcrud.com/api/8b1d5eae718848d2985828db9e6952c0/expenseData/${li.id}`)
+    .then(res=>itemList.removeChild(li))
+    .catch(err=>console.log(err));
+           
+            
+        }
+    }
+}
+
+function editItem(e) {
+    if (e.target.classList.contains('edit')) {
+        if (confirm('Are You sure?')) {
+             editingId = e.target.parentElement;
+             axios
+             .get(`https://crudcrud.com/api/8b1d5eae718848d2985828db9e6952c0/expenseData/${editingId.id}`)
+             .then((res) =>{amount.value=res.data.amn,
+                disc.value=res.data.dec,
+                catg.value=res.data.crt})
+                .catch(err=>console.log(err))
+
+             axios
+             .delete(`https://crudcrud.com/api/8b1d5eae718848d2985828db9e6952c0/expenseData/${editingId.id}`)
+           // item = JSON.parse(localStorage.getItem(editingId));
+            //console.log(editingId);
+            .then(res=> itemList.removeChild(editingId))
+           
+           
+
+        }
+    }
+
+}
+
+window.addEventListener("DOMContentLoaded",()=>{
+    axios
+    .get("https://crudcrud.com/api/8b1d5eae718848d2985828db9e6952c0/expenseData")
+.then((res)=>{
+    for(var i=0;i<res.data.length;i++){
+       showData(res.data[i])
+    }
+})
+.catch(err=>console.log(err));})
+
+function showData(e){
+
+    var newItem = e.amn;
+    var newDes = e.dec;
+    var newCat =e.crt;
 
     var li = document.createElement('li');
-    li.id = user.ides;
+    li.id=e._id
     li.className = 'list-group-item';
     li.appendChild(document.createTextNode(newItem + " " + newDes + " " + newCat));
     var deleteBtn = document.createElement('button');
@@ -36,48 +106,6 @@ function addItem(e) {
     btnEdit.appendChild(document.createTextNode('Edit Details'));
     li.appendChild(btnEdit);
     itemList.append(li);
-   // document.form.reset();
-    //if (editingId === null) {
-      //  localStorage.setItem(user.ides, JSON.stringify(user));
-        //editingId=user.ides;
-    //} else {
-      //  localStorage.setItem(editingId, JSON.stringify(user));
-        //editingId = null;
-    //}
 
-    axios
-    .post("https://crudcrud.com/api/a56c386d6538447592dbc098c6debb3a/expenseData",user)
-   .then(res =>console.log(res))
-   .catch(err=>console.log(err));
-    console.log(user);
-
-form.reset();
-}
-
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        if (confirm('Are You sure?')) {
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-            localStorage.removeItem("User");
-        }
-    }
-}
-
-function editItem(e) {
-    if (e.target.classList.contains('edit')) {
-        if (confirm('Are You sure?')) {
-             editingId = e.target.parentElement.id;
-             
-            item = JSON.parse(localStorage.getItem(editingId));
-            console.log(editingId);
-            amount.value=item.amn;
-            disc.value = item.dec;
-            catg.value = item.crt;
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
-
-        }
-    }
 
 }
